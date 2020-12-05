4d5a3c78ad13afad78836a21761140dd990a8f98
sakaokimiaki <sakaokimiaki33@yahoo.co.jp>
2020-12-05 18:04:45 +0900

    Initial commit

---
+++ laravel/resources/views/auth/register.blade.php
@@ -0,0 +1,9 @@
+@extends('app')
+
+@section('title', 'ユーザー登録')
+
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="mx-auto col col-12 col-sm-11 col-md-9 col-lg-7 col-xl-6">
+      
